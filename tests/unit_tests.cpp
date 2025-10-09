e321fcde1b07759f9fcc77fed9c1730a4fefe01c
VladKonovalenko2024 <vladyslav.konovalenko.ki.2022@lpnu.ua>
2025-10-09 21:39:15 +0300

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -12,8 +12,3 @@ TEST(BasicAddition, HandlesNegativeAndPositive) {
 TEST(BasicAddition, HandlesZeros) {
     EXPECT_EQ(add(0, 0), 0);
 }
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
